2614b7a845b025244cb3e730df5df12d8bee1998
Your Name <you@example.com>
2024-01-09 23:41:39 +0100

    auto

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
+# we can see here that 98 loads as 1. that means that there is a 0
+# a loads as 0 but if we do a power 98 we will get load load power load
+# but we have load load load power. so its a ** b
+# if we do a **b + 98 we load 98 after the power operation but load 98 preceed
 def magic_calculation(a, b):
     return 98 + a ** b
