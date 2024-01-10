a3a20c03f26b9676cd000805f6c3d566545bc120
Your Name <you@example.com>
2024-01-10 12:01:42 +0100

    auto

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last = number % 10
 if number < 0:
-    last = -(-number % 10) 
+    last = -(-number % 10)
 if last == 0:
     comp = "and is 0"
 elif last > 5:
