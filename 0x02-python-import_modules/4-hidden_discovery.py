792524a6a898ee75c48fcbd09b6f289e57eadb36
Your Name <you@example.com>
2024-01-10 22:26:17 +0100

    auto

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     listf = dir(hidden_4)
-    for e in listf:
-        if e[0] != '_' and e[1] != '_':
-            print(e)
+    for element in listf:
+        if element[0] != '_' and element[1] != '_':
+            print("{}".format(element))
