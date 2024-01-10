b94ada7aa8e0662aadfd367cf60f5524f407bf5f
Your Name <you@example.com>
2024-01-10 12:49:52 +0100

    auto

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(99):
-    print("{:02d} ,".format(i), end='')
-print(" {:02d}".format(99))
+    print("{:02d}, ".format(i), end='')
+print("{:02d}".format(99))
 
 # 2 for number of spaces 0 for filling blanks with 0
 # if we use an if  and else we have 3 loops wtf
