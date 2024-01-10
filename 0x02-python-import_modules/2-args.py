97e4e3ea237f0c1fd6b92f3bd01e09ceac108fbb
Your Name <you@example.com>
2024-01-10 20:04:45 +0100

    auto

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-lenv = len(sys.argv)
-print("{} arguments".format(lenv - 1), end="")
-if lenv == 1:
-    print(".")
-else:
-    print(":")
-    for i in range(1, lenv):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    lenv = len(sys.argv)
+    print("{} arguments".format(lenv - 1), end="")
+    if lenv == 1:
+        print(".")
+    else:
+        print(":")
+        for i in range(1, lenv):
+            print("{}: {}".format(i, sys.argv[i]))
