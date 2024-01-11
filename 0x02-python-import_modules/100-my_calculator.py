c0a8dc6db725c143fe69219ea3fce7266cf629ee
Your Name <you@example.com>
2024-01-11 11:06:31 +0100

    auto

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-from sys import argv, exit
-from calculator_1 import add, mul, div, sub
-if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-a = int(argv[1])
-b = int(argv[3])
-operator = argv[2]
-match operator:
-    case "+":
-        func = add
-    case "-":
-        func = sub
-    case "*":
-        func = mul
-    case "/":
-        func = div
-    case _:
-        print("Unknown operator. Available operators: +, -, * and /")
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, mul, div, sub
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-print("{} {} {} = {}".format(a, operator, b, func(a, b)))
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+    match operator:
+        case "+":
+            func = add
+        case "-":
+            func = sub
+        case "*":
+            func = mul
+        case "/":
+            func = div
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    print("{} {} {} = {}".format(a, operator, b, func(a, b)))
