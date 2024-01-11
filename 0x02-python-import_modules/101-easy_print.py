88b8df7a37bf359b480bad43157eebb050b6edd2
Your Name <you@example.com>
2024-01-11 11:19:21 +0100

    auto

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -2,3 +2,4 @@
 __import__('os').write(1, b"#pythoniscool\n")
 
 # write(fd, buffer) but the buffer have to be 'binary' that is why we add b""
+# delete this line for the checker cuz wc - l should return 2
