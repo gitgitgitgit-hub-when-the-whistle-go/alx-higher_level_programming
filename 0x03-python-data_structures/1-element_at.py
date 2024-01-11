cc2350bdb7cd2bdf587eebe85a703387297ffe82
Your Name <you@example.com>
2024-01-11 20:53:40 +0100

    auto

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,4 +6,4 @@ def element_at(my_list, idx):
         return None
     return my_list[idx]
 
-#return blabla[idx] donne blabla[index] meme si blabla n est pas defini
+# return blabla[idx] donne blabla[index] meme si blabla n est pas defini
