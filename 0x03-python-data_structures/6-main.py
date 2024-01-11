267792d060fc23ad69bcef1dd38a2d73edec057b
Your Name <you@example.com>
2024-01-11 22:05:48 +0100

    auto

---
+++ 0x03-python-data_structures/6-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+pmi = __import__('6-print_matrix_integer').print_matrix_integer
+mat1 = [[]]
+mat2 = [
+    [1, 2, 3],
+    [2, 3]]
+pmi(mat1)
+pmi(mat2)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for list in matrix:
-        for e in list:
-            print("{:d}".format(e), end='')
+        for e in range(len(list)):
+            print("{:d}".format(list[e]), end='')
+            if e != len(list) - 1:
+                print(" ", end='')
         print("")
