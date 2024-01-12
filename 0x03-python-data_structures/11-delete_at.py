f4e51beff896041a2a313713d30ad87319733718
Your Name <you@example.com>
2024-01-12 17:02:24 +0100

    auto

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,5 +4,10 @@ def delete_at(my_list=[], idx=0):
         return my_list
     if idx >= len(my_list):
         return my_list
-    my_list = my_list[:idx] + my_list[idx + 1:]
+    del my_list[idx]
     return my_list
+
+# if you use my_list = my_list[:idx] + my_list[idx + 1:]
+# you change only the new list not the original list
+# if you use a loop some problem may occur if idx is in the edges.
+# del seems the only solution
