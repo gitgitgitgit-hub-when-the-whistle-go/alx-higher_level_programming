b1d7d7dd34580bbc53804b8dadf4c825d916c567
Your Name <you@example.com>
2024-01-12 13:42:51 +0100

    auto

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,3 +8,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     while len(y) < 3:
         y += (0,)
     return x[0] + y[0], x[1] + y[1]
+
+# .copy and .append don't work on tuple. <aybe all the list methods??
+# to append use += tuple. be careful (11) is not a tuple (11, ) is one.
