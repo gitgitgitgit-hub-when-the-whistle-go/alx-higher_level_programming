89575f934d966fea4dd171048c806ea2436d8a1b
Your Name <you@example.com>
2024-01-18 23:57:37 +0100

    auto

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    l = len(my_list)
-    for i in range(l):
+    le = len(my_list)
+    for i in range(le):
         if my_list[i] == search:
             my_list[i] = replace
