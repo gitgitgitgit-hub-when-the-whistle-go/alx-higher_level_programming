d5469ca6e2f8b35266b9e1b7f68af9ea72f35a4a
Your Name <you@example.com>
2024-01-18 23:35:57 +0100

    auto

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 sms = __import__('0-square_matrix_simple').square_matrix_simple
-mat = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+mat = [10]
 print(sms(mat))
 print(mat)
--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     newmatrix = []
     for lis in matrix:
