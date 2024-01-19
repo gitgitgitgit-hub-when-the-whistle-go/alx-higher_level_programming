4ab3bba55d4407eb001ebe3893487eec5941f0f2
Your Name <you@example.com>
2024-01-19 16:02:28 +0100

    auto

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
     max = None
     # max contains the key and not the max value
     for e in a_dictionary:
