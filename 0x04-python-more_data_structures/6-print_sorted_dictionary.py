7991e5297ec67887322381f7049623df47b0e701
Your Name <you@example.com>
2024-01-19 12:58:18 +0100

    auto

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(dic):
-    lis = dic.keys()
+    lis = [x for x in dic.keys()]
     lis.sort()
     for key in lis:
         print(dic[key])
+# Warning dic.keys() don't return a list. it return a dict keys weird thing
