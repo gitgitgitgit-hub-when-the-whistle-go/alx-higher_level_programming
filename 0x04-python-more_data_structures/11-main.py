9658fb3bb4890822b2aa7171493760d798c53e94
Your Name <you@example.com>
2024-01-19 16:13:28 +0100

    auto

--- 0x04-python-more_data_structures/11-main.py
+++ 0x04-python-more_data_structures/11-main.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 mlm = __import__('11-multiply_list_map').multiply_list_map
-list = None
+list = [1, 2, 5, 6, 7, 89, 10]
 print(mlm(list, 10))
+
+# pycodestyle don't like lambda outside of map and requires to use def instead
+# illuminarti conf1rm3d ??!?!?!?
+# map can be used inside of map
