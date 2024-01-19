66434985028c456672c519d02e5d28a808fc582d
Your Name <you@example.com>
2024-01-19 14:03:51 +0100

    auto

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,3 +3,5 @@ def simple_delete(a_dictionary, key=""):
     a_dictionary.update({key: "can't if no key with del and poop x) 3bad5me"})
     a_dictionary.pop(key)
     return a_dictionary
+
+# I will create it for you to delete it body <34546 we can be bros
