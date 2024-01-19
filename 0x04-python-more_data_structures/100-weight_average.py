1bb1e63a94ac41a36be5349e4b03011a71592969
Your Name <you@example.com>
2024-01-20 00:03:48 +0100

    auto

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list = []:
+    if my_list == []:
         return 0
     bast = 0
     ma9am = 0
     for element in my_list:
-        bast = element[0] * element[1]
+        bast += element[0] * element[1]
         ma9am += element[1]
     return bast / ma9am
