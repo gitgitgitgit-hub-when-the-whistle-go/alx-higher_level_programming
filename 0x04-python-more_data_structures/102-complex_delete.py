8bc3b0e2ae4dec8704570c7649920ec992f64111
Your Name <you@example.com>
2024-01-20 00:06:49 +0100

    auto

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,3 +5,7 @@ def complex_delete(a_dict, value):
         if a_dict[element] == value:
             del a_dict[element]
     return a_dict
+
+# I had an error while modifying a_dict apparently the dictionary should not
+# change during iteration. That is the reason why I created a copy, so the
+# copy can stay intact
