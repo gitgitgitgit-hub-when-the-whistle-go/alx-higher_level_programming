98d84939736d20aeb47e3ef5b75648e6505dd0dc
Your Name <you@example.com>
2024-01-19 14:02:47 +0100

    auto

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.update({key: "blabla del and pop didn't works 3bad5me"})
+    a_dictionary.update({key: "can't if no key with del and poop x) 3bad5me"})
     a_dictionary.pop(key)
     return a_dictionary
