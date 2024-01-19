fd61c1ea1a4c85d28e82c0ff2f5a95091b965238
Your Name <you@example.com>
2024-01-19 17:58:00 +0100

    auto

---
+++ 0x04-python-more_data_structures/12-main.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+rti = __import__('12-roman_to_int').roman_to_int
+print("{} {}".format(rti('M'), rti('LXXXVII')))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    for e in roman_string:
+        match e:
+            case 'M':
+                sum += 1000
+            case 'D':
+                sum += 500
+            case 'C':
+                sum += 100
+            case 'L':
+                sum += 50
+            case 'X':
+                sum += 10
+            case 'V':
+                sum += 5
+            case 'I':
+                sum += 1
+            case _:
+                return None
+    return sum
