7b8a16ac597d60e14c5e8da892e3efda56d96635
Your Name <you@example.com>
2024-01-20 17:53:25 +0100

    auto

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
+    try:
+        for e in range(x):
+            print("{:d}",format(my_list[e]), end='')
+            printed += 1
+    except Exception:
+        return printed
+    finally:
+        print("")
+        return printed
