7308cfef961d2965455ed8393963add36c0af789
Your Name <you@example.com>
2024-01-20 18:47:53 +0100

    auto

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        stderr.write("Exception: Unknown format \
+code 'd' for object of type 'str'\n")
+        return False
+
+# if you import leave 2 lines between the import and the next def
+# no problem if it's not a def
