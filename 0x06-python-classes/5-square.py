d4f7cf992c06cb5d758a3d30e17204461339ee8b
Your Name <you@example.com>
2024-01-21 15:13:29 +0100

    auto

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+""" I realllllyyyyyy hate this docstring thing frfrffrfr """
+
+
+class Square:
+    """ blablabla docstring is a waste of time
+    Args:
+        size: the frakin size
+    Attributes:
+        __size: sdlkfjslkdflksdjflks
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        """ dbfdlkjldkfjgldkfg """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ sdfsdfs lksdjflks j """
+        if self.size == 0:
+            print("")
+            exit
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end='')
+            print("")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
