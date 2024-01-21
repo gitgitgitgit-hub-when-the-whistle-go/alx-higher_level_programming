89388ce5a4d49f98b736961838abd8aaa520f597
Your Name <you@example.com>
2024-01-21 13:15:19 +0100

    auto

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" I realllllyyyyyy hate this docstring thing frfrffrfr """
+
+
+class Square:
+    """ blablabla doctring is a waste of time
+    Args:
+        size: the frakin size
+    Attributes:
+        __size: sdlkfjslkdflksdjflks
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError
+        if size < 0:
+            raise ValueError
+        self.__size = size
