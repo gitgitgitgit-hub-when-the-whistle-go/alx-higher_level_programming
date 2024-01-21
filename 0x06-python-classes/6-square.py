dd79b0f24173f935c59e81bf9b233efc1bd0a0aa
Your Name <you@example.com>
2024-01-21 22:34:51 +0100

    auto

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,4 +56,6 @@ class Square:
         elif not isinstance(position[1], int) \
                 or not isinstance(position[0], int):
             raise TypeError('position must be a tuple of 2 positive integers')
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = position
