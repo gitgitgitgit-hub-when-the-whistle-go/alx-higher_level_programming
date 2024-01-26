be756e1d3f4a5d88a3b33cc4887993ec609e5d08
Your Name <you@example.com>
2024-01-26 11:11:50 +0100

    auto

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Module doc """
+
+
+def matrix_divided(matrix, div):
+    """
+    Args:
+        matrix: a matrix xD ???
+        div: it's self explanatory ffs
+    Raises:
+        TypeError: if element in matrix is not integer or float
+        TypeError: if the matrix has rows not in the same size
+        TypeError: if div is not int or float
+        ZeroDivisionError: if div == 0
+    Returns:
+        a matrix with all the elemets form the original one divided by
+        div rounded to 2 decimal
+    """
+    # we can see here that zerodiv and not integer errors can be checked while
+    # the func is executing. let's check the 2 others at the beginning xD
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError('div must be a number')
+    # we need to check for matrix empty or None or priest
+    if matrix is None or matrix == []:
+        return None
+    lenght = len(matrix[0])
+    for e in matrix:
+        if len(e) != lenght:
+            raise TypeError('Each row of the matrix must have the same size')
+
+    # if we copy only the matrix and modify internal lists in the new mat
+    # we will change also those in the original one so we need to copy them
+    new_matrix = [lis.copy() for lis in matrix]
+    for lis in new_matrix:
+        for i in range(lenght):
+            try:
+                lis[i] /= div
+            except ZeroDivisionError:
+                raise ZeroDivisionError('division by zero')
+            except TypeError:
+                # the only type error now is the one from dividing non integers
+                raise TypeError('matrix must be a matrix (list of lists) of \
+                    integers/floats')
+    return new_matrix
+
+
+if __name__ == "__main__":
+    from doctest import testfile
+    testfile('tests/2-matrix_divided.txt')
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -6,6 +6,9 @@ def func():
     TRaceback blbabalbalbalbalb
     ...
     Error: there was an error (copy it from the interpreter xD)
+
+    don't forget the test for missing arguments
+
 and then at the end of our file we add:
 if __name__ == "__main__":
     import doctest
@@ -18,4 +21,6 @@ python3 myfile.py
 python3 -m myfile -v  # -v is for having a the details in case of everything succesful
 python3 -m doctest -v myfile.py # here we invoc the module doctest and we don't need the if name == main block
 
-
+we can write all the doctest in another fileand then in our module add:
+from doctest import testfile
+    testfile('path/file.txt)
