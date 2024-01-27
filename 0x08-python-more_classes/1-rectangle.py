68a78cfb1f23a97cb3ad153b82de483738932ce6
Your Name <you@example.com>
2024-01-27 19:11:23 +0100

    auto

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,14 +24,14 @@ class Rectangle:
     def width(self, width):
         if not isinstance(width, int):
             raise TypeError('width must be an integer')
-        if width >= 0:
+        if width < 0:
             raise ValueError('width must be >= 0')
         self.__width = width
 
     @height.setter
-    def height(self, width):
+    def height(self, height):
         if not isinstance(height, int):
             raise TypeError('height must be an integer')
-        if height >= 0:
+        if height < 0:
             raise ValueError('height must be >= 0')
         self.__height = height
