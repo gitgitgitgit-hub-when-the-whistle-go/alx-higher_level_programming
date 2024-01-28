2c84f1755043d4d315ef2d3655427f9b29b8f1ff
Your Name <you@example.com>
2024-01-28 22:40:05 +0100

    auto

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,12 +7,11 @@ class BaseGeometry:
     def area(self):
         raise Exception('area() is not implemented')
 
-    @classmethod
     def integer_validator(self, name, value):
         """ k ur legit """
         if not isinstance(name, str):
             raise TypeError
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
