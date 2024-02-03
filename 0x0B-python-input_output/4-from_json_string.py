7056ae1b5bea4d5f17034f29df0501aabfeabaef
Your Name <you@example.com>
2024-02-03 19:22:16 +0100

    auto

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,9 +2,9 @@
 """ returns an object (Python data structure) represented by a JSON string """
 
 
-from json import load
+import json
 
 
 def from_json_string(jsonObj):
     """ as mod doc says xD """
-    return load(jsonObj)
+    return json.loads(jsonObj)
