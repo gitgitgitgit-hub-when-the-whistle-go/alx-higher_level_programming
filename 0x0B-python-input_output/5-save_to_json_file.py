b88e8688afb465fe66777dee082def03f2b8678f
Your Name <you@example.com>
2024-02-03 19:26:41 +0100

    auto

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a function that returns the JSON representation of an object (file) """
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ R T F moduledoc """
+    with open(filename, 'r', encoding="utf-8") as f:
+        json.dump(my_obj, f)
