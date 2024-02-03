abdb8d0e4794e0528298115e51ae88478be13f2a
Your Name <you@example.com>
2024-02-03 23:15:08 +0100

    auto

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ mixes the json from add_item.json with the argv form this module """
 
-
-import sys
-
-
-load_file = __import__('6-load_from_json_file').load_from_json_file
-save_file = __import__('5-save_to_json_file').save_to_json_file
-
-lis = sys.argv[1:]
-lis.extend(load_file("add_item.json"))
-save_file(lis, "add_item.json")
+if __name__ == "__main__":
+    import sys
+    load_file = __import__('6-load_from_json_file').load_from_json_file
+    save_file = __import__('5-save_to_json_file').save_to_json_file
+    try:
+        lis = load_file("add_item.json")
+    except FileNotFoundError:
+        lis = []
+        print("not found")
+    lis.extend(sys.argv[1:])
+    save_file(lis, "add_item.json")
