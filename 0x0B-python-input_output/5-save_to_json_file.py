6b0b1e3350c8751e9f3ff0db18bf16608fd238e4
Your Name <you@example.com>
2024-02-03 19:29:21 +0100

    auto

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ R T F moduledoc """
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, 'r+', encoding="utf-8") as f:
         json.dump(my_obj, f)
+
