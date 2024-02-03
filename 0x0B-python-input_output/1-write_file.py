cac715603c9482453125e619eff4b9503186b0a2
Your Name <you@example.com>
2024-02-03 18:43:39 +0100

    auto

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,9 @@ number of characters written """
 
 
 def write_file(filename='', text=""):
+    """ a function that writes a string to a text file (UTF8) and returns the\
+    number of characters written """
+
     with open(filename, 'w', encoding="utf-8") as f:
         chars_printed = f.write(text)
     return chars_printed
