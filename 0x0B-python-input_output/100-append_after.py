2dfb6212672f9cc646db69ed854cd196fe3e6a15
Your Name <you@example.com>
2024-02-04 23:07:52 +0100

    auto

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 
 
 def append_after(filename='', search_string='', new_string=''):
+    """ docstring fails the checker again f#@$@#$Q@#W$@ """
     lines = []
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
